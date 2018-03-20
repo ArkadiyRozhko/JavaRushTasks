2b4a702d8633fe1d6a61c92a8c7c88c814257cf1
Аркадій <arkasha9@gmail.com>
2018-03-20 16:47:26 +0200

    Задача 1411 вирішена

--- 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -14,8 +14,19 @@ public class Solution {
         String key = null;
 
         //тут цикл по чтению ключей, пункт 1
+        while (true)
         {
-            //создаем объект, пункт 2
+            key=reader.readLine();
+            if (key.equals("user")) {
+                person=new Person.User();
+            }else if (key.equals("loser")){
+                person=new Person.Loser();
+            }else if (key.equals("coder")){
+                person=new Person.Coder();
+            }else if (key.equals("proger")){
+                person=new  Person.Proger();
+            }else break;
+
 
             doWork(person); //вызываем doWork
 
@@ -23,6 +34,17 @@ public class Solution {
     }
 
     public static void doWork(Person person) {
-        // пункт 3
+        if (person instanceof Person.User){
+            ((Person.User) person).live();
+        }
+        if (person instanceof Person.Loser) {
+            ((Person.Loser) person).doNothing();
+        }
+        if (person instanceof Person.Coder){
+            ((Person.Coder) person).coding();
+        }
+        if (person instanceof Person.Proger){
+            ((Person.Proger) person).enjoy();
+        }
     }
 }
