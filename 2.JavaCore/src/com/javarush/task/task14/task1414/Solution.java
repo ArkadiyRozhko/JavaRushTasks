beffb637b86e8a1a4f96b35530243dd8ac3b6520
Аркадій <arkasha9@gmail.com>
2018-03-21 11:29:15 +0200

    Задача 1414 вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,70 @@
+package com.javarush.task.task14.task1414;
+
+/* 
+MovieFactory
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //ввести с консоли несколько ключей (строк), пункт 7
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Movie movie=null;
+        String key = null;
+        while (true){
+            key=reader.readLine();
+            movie=MovieFactory.getMovie(key);
+            if (!(key.equals("soapOpera")||key.equals("cartoon")||key.equals("thriller"))){
+                break;
+            }
+            System.out.println(movie.getClass().getSimpleName());
+        }
+
+        /*
+8 Создать переменную movie класса Movie и для каждой введенной строки(ключа):
+8.1 получить объект используя MovieFactory.getMovie и присвоить его переменной movie
+8.2 вывести на экран movie.getClass().getSimpleName()
+        */
+
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            //создание объекта SoapOpera (мыльная опера) для ключа "soapOpera"
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            }
+            if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            }
+            if ("thriller".equals(key)){
+                movie = new Thriller();
+            }
+
+            //напишите тут ваш код, пункты 5,6
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie{
+
+    }
+
+    static class Thriller extends Movie{
+
+    }
+
+    //Напишите тут ваши классы, пункт 3
+}
