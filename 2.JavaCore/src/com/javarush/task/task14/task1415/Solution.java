e97907cd88c99390c40ec101dc6677f4c2fa02c9
Аркадій <arkasha9@gmail.com>
2018-03-21 11:35:54 +0200

    Задача 1415 вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1415/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task14.task1415;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Клининговый центр
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        List<Apartment> apartments = new ArrayList<Apartment>();
+        apartments.add(new Apt1Room());
+        apartments.add(new Apt2Room());
+        apartments.add(new Apt3Room());
+
+        cleanAllApartments(apartments);
+    }
+
+    public static void cleanAllApartments(List<Apartment> apartments) {
+        //написать тут вашу реализацию пунктов 1-4
+        for (Apartment ap:apartments
+             ) {
+            if (ap instanceof Apt1Room ) {
+                ((Apt1Room) ap).clean1Room();
+            }else if (ap instanceof Apt2Room){
+                ((Apt2Room) ap).clean2Rooms();
+            }else if (ap instanceof Apt3Room){
+                ((Apt3Room) ap).clean3Rooms();
+            }
+        }
+    }
+
+    static interface Apartment {
+    }
+
+    static class Apt1Room implements Apartment {
+        void clean1Room() {
+            System.out.println("1 room is cleaned");
+        }
+    }
+
+    static class Apt2Room implements Apartment {
+        void clean2Rooms() {
+            System.out.println("2 rooms are cleaned");
+        }
+    }
+
+    static class Apt3Room implements Apartment {
+        void clean3Rooms() {
+            System.out.println("3 rooms are cleaned");
+        }
+    }
+}
