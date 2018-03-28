b29141732b5bfc3c2d493d712e7c990cc2a68cd8
Аркадій <arkasha9@gmail.com>
2018-03-28 18:15:58 +0300

    Задача 1523 вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    private Solution(){
+
+    }
+    Solution(String s){
+
+    }
+    public Solution(int i) {
+    }
+    protected Solution(int i,String s){
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution(String s) {
+        super(s);
+    }
+
+    public SubSolution(int i) {
+        super(i);
+    }
+
+    protected SubSolution(int i, String s) {
+        super(i, s);
+    }
+}
