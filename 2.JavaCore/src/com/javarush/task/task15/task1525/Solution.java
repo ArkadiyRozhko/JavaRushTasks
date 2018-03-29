5c7e69fe8ce44b69af04f26e6685ad96cd2ceb34
Аркадій <arkasha9@gmail.com>
2018-03-29 08:45:21 +0300

    Задача 1525 вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1525/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task15.task1525;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Файл в статическом блоке
+*/
+
+public class Solution {
+    public static List<String> lines = new ArrayList<String>();
+    static {
+
+        try {
+            try (BufferedReader reader=new BufferedReader(new InputStreamReader(new FileInputStream(Statics.FILE_NAME)))){
+                String line;
+                while ((line=reader.readLine())!=null){
+                    lines.add(line);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lines);
+    }
+}
