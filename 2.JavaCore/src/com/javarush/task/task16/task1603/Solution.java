db8cd85a7d031c9d4880878bc06614c8cf4c6ac9
Аркадій <arkasha9@gmail.com>
2018-04-12 15:53:45 +0300

    Задача 1603 вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        //Add your code here - добавьте свой код тут
+        SpecialThread specialThread1=new SpecialThread();
+        SpecialThread specialThread2=new SpecialThread();
+        SpecialThread specialThread3=new SpecialThread();
+        SpecialThread specialThread4=new SpecialThread();
+        SpecialThread specialThread5=new SpecialThread();
+        Thread thread1=new Thread(specialThread1);
+        Thread thread2=new Thread(specialThread2);
+        Thread thread3=new Thread(specialThread3);
+        Thread thread4=new Thread(specialThread4);
+        Thread thread5=new Thread(specialThread5);
+        list.add(thread1);
+        list.add(thread2);
+        list.add(thread3);
+        list.add(thread4);
+        list.add(thread5);
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
