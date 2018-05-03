7fb09a3aa57a7d9a6c9fd8712b887517eb08dfdc
Аркадій <arkasha9@gmail.com>
2018-05-03 17:29:56 +0300

    Задача 1619 вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        TestThread.isCancel=true;
+    }
+
+    public static class TestThread implements Runnable {
+        static boolean isCancel = false;
+        public void run() {
+            while (!isCancel) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
