2a6293b92a16f37ae9585fbe6e46bc187675b168
Аркадій <arkasha9@gmail.com>
2018-05-09 14:54:53 +0300

    Задача 1620 вирішена

--- 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task15/task1527/Solution.java
@@ -1,11 +1,8 @@
 package com.javarush.task.task15.task1527;
 
-import com.sun.javafx.collections.MappingChange;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
