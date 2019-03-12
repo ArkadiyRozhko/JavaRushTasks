06e6c23d701cd5bf694b009164e054de4119489e
Аркадій <arkasha9@gmail.com>
2019-03-12 18:05:35 +0200

    Задача 1918 не вирішена (Умова)

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -22,8 +22,11 @@ public class Solution {
                 result+=(char)c;
             }
         }
-        tagList(result,args[0]);
-        System.out.println(result);
+        ArrayList<String> list=tagList(result,args[0]);
+        for (String s:list
+             ) {
+            System.out.println(s);
+        }
 
     }
     public static ArrayList<String> tagList(String str, String tag){
@@ -45,16 +48,19 @@ public class Solution {
         String beginTag="<"+tag;
         String endTag="</"+tag+">";
 
-        int lastIndexOffTag=str.indexOf(endTag);
         int firstIndexOfTag=str.indexOf(beginTag);
-        while (firstIndexOfTag<lastIndexOffTag){
-            firstIndexOfTag=str.indexOf(beginTag,firstIndexOfTag+1);
+        int lastIndexOffTag=str.indexOf(endTag);
+
+
+        while (firstIndexOfTag<lastIndexOffTag&&str.indexOf(beginTag,firstIndexOfTag+1)!=-1){
+
+                firstIndexOfTag=str.indexOf(beginTag,firstIndexOfTag+1);
             count++;
         }
-        for (int i = count; i >0 ; i--) {
+        for (int i = count-1; i >0 ; i--) {
             lastIndexOffTag=str.indexOf(endTag,lastIndexOffTag+1);
         }
-        return lastIndexOffTag;
+        return lastIndexOffTag+endTag.length();
 
     }
 }
