c19d284e0b4dfc44592d428cfc6b707181688184
Аркадій <arkasha9@gmail.com>
2019-03-18 15:28:09 +0200

    Задача 1918 вирішена

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -8,7 +8,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
 
 public class Solution {
     public static void main(String[] args) throws IOException {
@@ -21,62 +26,70 @@ public class Solution {
             while ((c=fileReader.read())!=-1){
                 result+=(char)c;
             }
-        }        
-        ArrayList<String> list=tagList(result,args[0]);
-        for (String s:list
-             ) {
-            System.out.println(s);
         }
 
+        Document html=Jsoup.parse(result,"", Parser.xmlParser());
+        Elements element = html.select(args[0]);
+        for (Element elements : element){
+            System.out.println(elements);
+        }
+
+
+
+//        System.out.println(result);
+//        result=result.replace("\n","").replace("\r","");
+//        System.out.println(result);
+//        ArrayList<String> list=tagList(result,args[0]);
+//        for (String s:list
+//             ) {
+//            System.out.println(s);
+//        }
+
     }
-    public static ArrayList<String> tagList(String str, String tag){
-        String beginTag="<"+tag;
-        String endTag="</"+tag+">";
-        ArrayList<String>strList=new ArrayList<>();
-        String str1=str;
-        //StringBuilder builder=new StringBuilder(str);
-        while (!(str1.isEmpty())&&str1.indexOf(beginTag)!=-1){
-            int firstIndex=str1.indexOf(beginTag);
-            int endIndex=lastIndexOf(str1,tag);
-            strList.add(str1.substring(firstIndex,endIndex));
-            String temp;
-            String temp1;
-
-            if ((str1.charAt(firstIndex+beginTag.length()))=='>') {
-                temp=str1.substring(firstIndex+beginTag.length()+1,endIndex-endTag.length());
-            }else {
-                temp = str1.substring(firstIndex + beginTag.length(), endIndex - endTag.length());
-            }
-            temp1=str1.substring(endIndex);
-            str1=temp+temp1;
-
-
-//            str1=str1.substring(firstIndex+beginTag.length())+str1.substring(0,endIndex-endTag.length())+str1.substring(endIndex);
-//            String temp=str1.substring(0,endIndex-endTag.length());
-//            String temp1=str1.substring(endIndex);
+//    public static ArrayList<String> tagList(String str, String tag){
+//        String beginTag="<"+tag;
+//        String endTag="</"+tag+">";
+//        ArrayList<String>strList=new ArrayList<>();
+//        String str1=str;
+//        //StringBuilder builder=new StringBuilder(str);
+//        while (!(str1.isEmpty())&&str1.indexOf(beginTag)!=-1){
+//            int firstIndex=str1.indexOf(beginTag);
+//            int endIndex=lastIndexOf(str1,tag);
+//            strList.add(str1.substring(firstIndex,endIndex));
+//            String temp;
+//            String temp1;
+//
+//            if ((str1.charAt(firstIndex+beginTag.length()))=='>') {
+//                temp=str1.substring(firstIndex+beginTag.length()+1,endIndex-endTag.length());
+//            }else {
+//                temp = str1.substring(firstIndex + beginTag.length(), endIndex - endTag.length());
+//            }
+//            temp1=str1.substring(endIndex);
 //            str1=temp+temp1;
-
-        }
-        return  strList;
-    }
-    public static int lastIndexOf(String str,String tag){
-        int count=0;
-        String beginTag="<"+tag;
-        String endTag="</"+tag+">";
-
-        int firstIndexOfTag=str.indexOf(beginTag);
-        int lastIndexOffTag=str.indexOf(endTag);
-
-
-        while (firstIndexOfTag<lastIndexOffTag&&str.indexOf(beginTag,firstIndexOfTag+1)!=-1){
-
-                firstIndexOfTag=str.indexOf(beginTag,firstIndexOfTag+1);
-            count++;
-        }
-        for (int i = count-1; i >0 ; i--) {
-            lastIndexOffTag=str.indexOf(endTag,lastIndexOffTag+1);
-        }
-        return lastIndexOffTag+endTag.length();
-
-    }
+//
+//
+//
+//        }
+//        return  strList;
+//    }
+//    public static int lastIndexOf(String str,String tag){
+//        int count=0;
+//        String beginTag="<"+tag;
+//        String endTag="</"+tag+">";
+//
+//        int firstIndexOfTag=str.indexOf(beginTag);
+//        int lastIndexOffTag=str.indexOf(endTag);
+//
+//
+//        while (firstIndexOfTag<lastIndexOffTag&&str.indexOf(beginTag,firstIndexOfTag+1)!=-1){
+//
+//                firstIndexOfTag=str.indexOf(beginTag,firstIndexOfTag+1);
+//            count++;
+//        }
+//        for (int i = count-1; i >0 ; i--) {
+//            lastIndexOffTag=str.indexOf(endTag,lastIndexOffTag+1);
+//        }
+//        return lastIndexOffTag+endTag.length();
+//
+//    }
 }
