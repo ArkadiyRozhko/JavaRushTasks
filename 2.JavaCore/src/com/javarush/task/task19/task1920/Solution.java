43caab55991f718a1d0eb0c209d6ec299f645b10
Аркадій <arkasha9@gmail.com>
2019-03-19 18:12:25 +0200

    Задача 1920 не вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1920/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task19.task1920;
+
+/* 
+Самый богатый
+*/
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String,Double>Emloee=new TreeMap<>();
+        try(BufferedReader reader=new BufferedReader(new FileReader(args[0]))){
+            String line;
+            while ((line=reader.readLine())!=null){
+                String[]temp=line.split(" ");
+                if (Emloee.get(temp[0]) != null) {
+                    Emloee.put(temp[0],Double.parseDouble(temp[1])+Double.parseDouble(temp[1]));
+                }else {
+                    Emloee.put(temp[0],Double.parseDouble(temp[1]));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (Map.Entry item:Emloee.entrySet()
+             ) {
+            System.out.println(item.getKey());
+        }
+    }
+}
