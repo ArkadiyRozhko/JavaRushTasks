e9935b266838538f13663b1f5425fdb2d1c8edea
Аркадій <arkasha9@gmail.com>
2019-05-24 17:18:11 +0300

    Задача 1925 не вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task19.task1925;
+
+/* 
+Длинные слова
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String file1=args[0];
+        String file2=args[1];
+        try (BufferedReader reader=new BufferedReader(new FileReader(file1));
+             BufferedWriter writer=new BufferedWriter(new FileWriter(file2))){
+
+
+        }
+
+    }
+}
