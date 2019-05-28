86c71e8fd3deb621ff23fe6fa91f8442854f3d01
Аркадій <arkasha9@gmail.com>
2019-05-28 16:51:38 +0300

    Задача 1925 вирішена

--- 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1925/Solution.java
@@ -12,8 +12,19 @@ public class Solution {
         String file2=args[1];
         try (BufferedReader reader=new BufferedReader(new FileReader(file1));
              BufferedWriter writer=new BufferedWriter(new FileWriter(file2))){
-
-
+            String line="";
+            String[]words;
+            String result = "";
+            while ((line=reader.readLine())!=null){
+                words=line.split(" ");
+                for (int i = 0; i <words.length ; i++) {
+                    if (words[i].length()>6) {
+                        result+=words[i]+" ";
+                    }
+                }
+            }
+            String stringForWrite=result.trim();
+            writer.write(stringForWrite.replace(" ",","));
         }
 
     }
