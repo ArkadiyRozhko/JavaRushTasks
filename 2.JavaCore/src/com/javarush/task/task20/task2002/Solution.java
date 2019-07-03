aae9465beaf0dc7c228156093eb1e62bda05e94e
Аркадій <arkasha9@gmail.com>
2019-07-03 18:12:25 +0300

    Задача 2002 не вирішена

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2002/Solution.java
@@ -0,0 +1,105 @@
+package com.javarush.task.task20.task2002;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/* 
+Читаем и пишем в файл: JavaRush
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //you can find your_file_name.tmp in your TMP directory or adjust outputStream/inputStream according to your file's actual location
+        //вы можете найти your_file_name.tmp в папке TMP или исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+            File yourFile = File.createTempFile("your_file_name", null);
+            OutputStream outputStream = new FileOutputStream(yourFile);
+            InputStream inputStream = new FileInputStream(yourFile);
+
+            JavaRush javaRush = new JavaRush();
+            //initialize users field for the javaRush object here - инициализируйте поле users для объекта javaRush тут
+            User user=new User();
+            user.setFirstName("Вася");
+            user.setLastName("Пупкын");
+            user.setCountry(Country("Ukraine"));
+            user.setMale(true);
+            user.setBirthDate(new Date());
+            javaRush.users.add(user);
+            javaRush.save(outputStream);
+            outputStream.flush();
+
+            JavaRush loadedObject = new JavaRush();
+            loadedObject.load(inputStream);
+            //here check that the javaRush object is equal to the loadedObject object - проверьте тут, что javaRush и loadedObject равны
+
+            outputStream.close();
+            inputStream.close();
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something is wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something is wrong with the save/load method");
+        }
+    }
+
+    public static class JavaRush {
+        public List<User> users = new ArrayList<>();
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            PrintWriter writer=new PrintWriter(outputStream);
+            String isUsersExist=users.isEmpty()?"no":"yes";
+            writer.println(isUsersExist);
+            writer.flush();
+            if (isUsersExist.equals("yes")) {
+                for (int i = 0; i <users.size() ; i++) {
+                    writer.println(users.get(i).getFirstName());
+                    writer.println(users.get(i).getLastName());
+                    writer.println(users.get(i).getBirthDate().getTime());
+                    writer.println(users.get(i).isMale());
+                    writer.println(users.get(i).getCountry().getDisplayName());
+                    writer.flush();
+                }
+            }
+
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream));
+            String isUsersExist=reader.readLine();
+            if (isUsersExist.equals("yes")) {
+                while (reader.ready()){
+                    User user=new User();
+                    user.setFirstName(reader.readLine());
+                    user.setLastName(reader.readLine());
+                    user.setBirthDate(new Date(Long.parseLong(reader.readLine())));
+                    user.setMale(Boolean.valueOf(reader.readLine()));
+                    user.setCountry(User.Country.valueOf(reader.readLine()));
+                    users.add(user);
+                }
+
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            JavaRush javaRush = (JavaRush) o;
+
+            return users != null ? users.equals(javaRush.users) : javaRush.users == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            return users != null ? users.hashCode() : 0;
+        }
+    }
+}
