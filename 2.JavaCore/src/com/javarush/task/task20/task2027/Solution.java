a4602dd6201e69f203559bb7c735dd3b55679076
Ark <arkasha9@gmail.com>
2021-09-29 18:01:59 +0300

    2027 Вирішена

--- 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2027/Solution.java
@@ -15,9 +15,13 @@ public class Solution {
                 {'m', 'l', 'p', 'r', 'r', 'h'},
                 {'p', 'o', 'e', 'e', 'j', 'j'}
         };
-        System.out.println(crossword[0].length);
 
-        detectAllWords(crossword, "home", "same");
+        List<Word> words= detectAllWords(crossword, "home", "same");
+        for (Word word:words
+             ) {
+            System.out.println(word);
+        }
+
         /*
 Ожидаемый результат
 home - (5, 3) - (2, 0)
@@ -32,15 +36,122 @@ same - (1, 1) - (4, 1)
              ) {
             for (int i = 0; i <crossword.length ; i++) {
                 for (int j = 0; j <crossword[i].length ; j++) {
-                    if (crossword[i][j] == word.charAt(0)) {
-                        if (i+word.length()-1<=crossword[i].length){
+                    char ch=(char) crossword[i][j];
+                    char firstch=word.charAt(0);
+                    if ( ch== firstch) {
+                        if (j+word.length()-1<crossword[i].length){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i][j+k];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j+tmp.length-1,i);
+                                wordList.add(word1);
+                            }
+
+                        }
+                        if (j-word.length()+1>=0){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i][j-k];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j-tmp.length+1,i);
+                                wordList.add(word1);
+                            }
+
+                        }
+                        if (i+word.length()-1<crossword.length){
                             char [] tmp=new char[word.length()];
                             for (int k = 0; k <tmp.length; k++) {
                                 tmp[k]=(char) crossword[i+k][j];
                             }
-                            System.out.println(tmp);
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j,i+tmp.length-1);
+                                wordList.add(word1);
+                            }
 
                         }
+                        if (i-word.length()+1>=0){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i-k][j];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j,i-tmp.length+1);
+                                wordList.add(word1);
+                            }
+
+                        }
+                        if ((j+word.length()-1<crossword[i].length) && (i-word.length()+1>=0)){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i-k][j+k];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j+tmp.length-1,i-tmp.length+1);
+                                wordList.add(word1);
+                            }
+
+                        }
+                        if ((j+word.length()-1<crossword[i].length) && (i+word.length()-1<crossword.length)){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i+k][j+k];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j+tmp.length-1,i+tmp.length-1);
+                                wordList.add(word1);
+                            }
+
+                        }
+                        if ((j-word.length()+1>=0) && (i+word.length()-1<crossword.length)){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i+k][j-k];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j-tmp.length+1,i+tmp.length-1);
+                                wordList.add(word1);
+                            }
+
+                        }
+                        if ((j-word.length()+1>=0) && (i-word.length()+1>=0)){
+                            char [] tmp=new char[word.length()];
+                            for (int k = 0; k <tmp.length; k++) {
+                                tmp[k]=(char) crossword[i-k][j-k];
+                            }
+                            String wordfrommass=new String(tmp);
+                            if (word.equals(wordfrommass)){
+                                Word word1=new Word(wordfrommass);
+                                word1.setStartPoint(j,i);
+                                word1.setEndPoint(j-tmp.length+1,i-tmp.length+1);
+                                wordList.add(word1);
+                            }
+
+                        }
+
                     }
                 }
 
