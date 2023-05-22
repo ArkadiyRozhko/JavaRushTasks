99753b6b69c90e22d0304ef1012b9c822f43ebc1
Arkadii Rozhko <arkasha9@gmail.com>
2023-05-22 20:41:17 +0300

    Задача 2213 4 вирішена

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -12,7 +12,16 @@ public class Tetris {
         return figure;
     }
 
+    public void run() {
+
+    }
+    public void step(){
+
+    }
+
+
     public static void main(String[] args) {
         
     }
+
 }
