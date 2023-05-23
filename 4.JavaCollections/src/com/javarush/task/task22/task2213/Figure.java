4195414eb5d5262d426e6fa4668a42339d206b8d
arkadiy <arkasha9@gmail.com>
2023-05-23 16:52:51 +0300

    Задача 2213 10 вирішена

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -22,4 +22,16 @@ public class Figure {
         this.y = y;
         this.matrix = matrix;
     }
+    public void left(){
+
+    }
+    public void right(){}
+    public void down(){}
+    public void up(){}
+    public void rotate(){}
+    public void downMaximum(){}
+    public boolean isCurrentPositionAvailable(){
+        return true;
+    }
+    public void landed(){}
 }
